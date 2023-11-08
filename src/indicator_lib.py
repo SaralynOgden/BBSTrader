1394568a2f90bdcf6d38a6e40c9781ac892b64d4
Brady Six <bpsix646@gmail.com>
2023-11-07 23:13:45 -0800

    chore: resolve conflicts

--- src/indicator_lib.py
+++ src/indicator_lib.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import numpy as np
-=======
-import numpy
->>>>>>> bb7d06cf9f09feb1fc6d1100ddfe38bed3c74e76
 
 def calc_ema(dataframe, ema_size):
     """
@@ -34,7 +30,6 @@ def calc_ema(dataframe, ema_size):
         else:
             dataframe.loc[i, ema_name] = 0.00
     # Return modified dataframe
-<<<<<<< HEAD
     return dataframe
 
 # Function to calculate a crossover event between two EMAs
@@ -62,6 +57,4 @@ def ema_cross_calc(dataframe, ema_one, ema_two):
     dataframe = dataframe.drop(columns="position")
     dataframe = dataframe.drop(columns="pre_position")
     # return dataframe with  detected to the user
-=======
->>>>>>> bb7d06cf9f09feb1fc6d1100ddfe38bed3c74e76
     return dataframe
